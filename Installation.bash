2ae46f2bb86f9ec5717ef872e0f750a55dcb4bb6
ChristofSeiler <christof.seiler@gmail.com>
2016-10-15 10:01:39 -0700

    add INSTALL_DIR

--- Installation.bash
+++ Installation.bash
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+INSTALL_DIR=$SCRATCH
+cd $INSTALL_DIR
+
 # install bowtie2
 wget https://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.2.9/bowtie2-2.2.9-linux-x86_64.zip
 unzip bowtie2-2.2.9-linux-x86_64.zip
 echo "" >> .bash_profile
 echo "# add alignment binaries" >> .bash_profile
-echo "export PATH=~/bowtie2-2.2.9:\$PATH" >> ~/.bash_profile
+echo "export PATH=${INSTALL_DIR}/bowtie2-2.2.9:\$PATH" >> ~/.bash_profile
 
 # install tophat2
 wget https://ccb.jhu.edu/software/tophat/downloads/tophat-2.1.1.Linux_x86_64.tar.gz
 tar -xvzf tophat-2.1.1.Linux_x86_64.tar.gz
 mv tophat-2.1.1.Linux_x86_64 tophat-2.1.1
-echo "export PATH=~/tophat-2.1.1:\$PATH" >> ~/.bash_profile
+echo "export PATH=${INSTALL_DIR}/tophat-2.1.1:\$PATH" >> ~/.bash_profile
 
 # download reference genome: H. sapiens, NCBI GRCh38
-BOWTIE_INDEXES=$SCRATCH/Bowtie_Indexes
+BOWTIE_INDEXES=${INSTALL_DIR}/Bowtie_Indexes
 mkdir $BOWTIE_INDEXES
 cd $BOWTIE_INDEXES
 wget ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Homo_sapiens/NCBI/GRCh38/Homo_sapiens_NCBI_GRCh38.tar.gz
@@ -22,7 +25,7 @@ tar -zxvf Homo_sapiens_NCBI_GRCh38.tar.gz
 echo "export BOWTIE_INDEXES=$BOWTIE_INDEXES" >> ~/.bash_profile
 
 # download gene model
-GENE_MODEL=$SCRATCH/Gene_Model
+GENE_MODEL=${INSTALL_DIR}/Gene_Model
 mkdir $GENE_MODEL
 cd $GENE_MODEL
 wget ftp://ftp.ensembl.org/pub/release-86/gtf/homo_sapiens/Homo_sapiens.GRCh38.86.gtf.gz
