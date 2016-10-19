a74392395dde0a9aaeb78bfbba13a4c473b2e2b7
ChristofSeiler <christof.seiler@gmail.com>
2016-10-19 14:49:24 -0700

    added gene symbols

--- Installation.bash
+++ Installation.bash
@@ -50,4 +50,4 @@ echo "export STAR_INDEXES=$STAR_INDEXES" >> ~/.bash_profile
 
 # install R/Bioconductor packages
 module load R/3.3.0
-R -e "source(\"https://bioconductor.org/biocLite.R\"); biocLite(c(\"Rsamtools\",\"GenomicFeatures\",\"BiocParallel\",\"DESeq2\",\"TxDb.Hsapiens.UCSC.hg38.knownGene\",\"GenomicAlignments\",\"BiocParallel\",\"STRINGdb\",\"BioNet\",\"DLBCL\"),suppressUpdates=TRUE); install.packages(c(\"pheatmap\",\"RColorBrewer\",\"ggplot2\",\"stringr\",\"igraph\",\"visNetwork\"),repos='http://cran.us.r-project.org')"
+R -e "source(\"https://bioconductor.org/biocLite.R\"); biocLite(c(\"Rsamtools\",\"GenomicFeatures\",\"BiocParallel\",\"DESeq2\",\"TxDb.Hsapiens.UCSC.hg38.knownGene\",\"GenomicAlignments\",\"BiocParallel\",\"STRINGdb\",\"BioNet\",\"DLBCL\",\"org.Hs.eg.db\"),suppressUpdates=TRUE); install.packages(c(\"pheatmap\",\"RColorBrewer\",\"ggplot2\",\"stringr\",\"igraph\",\"visNetwork\"),repos='http://cran.us.r-project.org')"
