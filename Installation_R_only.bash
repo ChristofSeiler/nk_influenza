80d9c3a256a1b37c3149bcc0a1589382634e5fc1
ChristofSeiler <christof.seiler@gmail.com>
2016-10-19 17:28:39 -0700

    added R only installation

---
+++ Installation_R_only.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+INSTALL_DIR=$SCRATCH
+cd $INSTALL_DIR
+
+# install R/Bioconductor packages
+module load R/3.3.0
+R -e "source(\"https://bioconductor.org/biocLite.R\"); biocLite(c(\"Rsamtools\",\"GenomicFeatures\",\"BiocParallel\",\"DESeq2\",\"TxDb.Hsapiens.UCSC.hg38.knownGene\",\"GenomicAlignments\",\"BiocParallel\",\"STRINGdb\",\"BioNet\",\"DLBCL\",\"org.Hs.eg.db\"),suppressUpdates=TRUE); install.packages(c(\"pheatmap\",\"RColorBrewer\",\"ggplot2\",\"stringr\",\"igraph\",\"visNetwork\"),repos='http://cran.us.r-project.org')"
